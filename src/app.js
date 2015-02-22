6b0e4adc6f519fbbe453ffb442cd1ec75ea6ae9a
orudis <orudis@hotmail.es>
2015-02-22 13:35:27 +0100

    Fixed bug repeat title in select input component

--- src/app.js
+++ src/app.js
@@ -37,10 +37,10 @@ app.controller('appController', ['$scope','$http', function ($scope,$http) {
 																									console.log("botón clickado")
 																								   } },
 					
-					{key: 'iban',type: 'iban',col:'col-md-4',label: 'Nº cuenta cliente',placeholder: 'Texto',autofocus:'',required: true,name:"nombre",id:"id"  },
-					{key: 'nif',type: 'nifniecif',col:'col-md-4',label: 'NIF',textbutton:'Nif',placeholder: 'NIF',autofocus:'',required: true },
-					{key: 'nie',type: 'nifniecif',col:'col-md-4',label: 'NIE',textbutton:'Nie',placeholder: 'NIE',autofocus:'',required: true },
-					{key: 'cif',type: 'nifniecif',col:'col-md-4',label: 'CIF',textbutton:'Cif',placeholder: 'CIF',autofocus:'',required: true },
+					{key: 'iban',type: 'iban',col:'col-md-4',inputSize:"lg",typebutton:"warning",label: 'Nº cuenta cliente',placeholder: 'Texto',autofocus:'',required: true,name:"nombre",id:"id"  },
+					{key: 'nif',type: 'nifniecif',col:'col-md-4',inputSize:"sm",label: 'NIF',textbutton:'NIF',placeholder: 'NIF',autofocus:'',required: true },
+					{key: 'nie',type: 'nifniecif',col:'col-md-4',inputSize:"sm",label: 'NIE',textbutton:'NIE',placeholder: 'NIE',autofocus:'',required: true },
+					{key: 'cif',type: 'nifniecif',col:'col-md-4',inputSize:"sm",typebutton:"danger",label: 'CIF',textbutton:'Cif',placeholder: 'CIF',autofocus:'',required: true },
 	                {key: 'texto',type: 'text',default:'texto por defecto',
 						fieldListeners:{
 							onChange:function(value){
@@ -53,7 +53,7 @@ app.controller('appController', ['$scope','$http', function ($scope,$http) {
 								console.log("salida del control:"+value);
 							}
 						},col:'col-md-6',label: 'Texto',placeholder: 'Texto',autofocus:'',required: true },
-					{key: 'textobtn',type: 'textbutton',default:'texto por def.',showbutton:true,typebutton:'info',icon:'search',textbutton:'texto',
+					{key: 'textobtn',type: 'textbutton',inputSize:"lg",default:'texto por def.',showbutton:true,typebutton:'info',icon:'search',textbutton:'texto',
 						fieldListeners:{
 						    onClick:function(value){
 								console.log("click on button: "+value);
@@ -76,8 +76,8 @@ app.controller('appController', ['$scope','$http', function ($scope,$http) {
 					{key: 'url',type: 'url',col:'col-md-4',label: 'Url',placeholder: 'Url',autofocus:'',required: true },
 					{key: 'password',type: 'password',col:'col-md-4',label: 'Password',placeholder: 'Password',autofocus:'',required: true },
 				   
-					{key: 'ckeckbox',type: 'checkbox',col:'col-md-4',label: 'Checkbox',placeholder: 'Checkbox',autofocus:'',disabled:false,required: true },
-					{key: 'radio',type: 'radio',col:'col-md-4',label: 'Radio',options:[{"name":"perro","value":"1"},{"name":"gato","value":"2"}],placeholder: 'Checkbox',autofocus:'',required: true },
+					{key: 'ckeckbox',type: 'checkbox',inputSizeClass:"input-lg",col:'col-md-4',label: 'Checkbox',placeholder: 'Checkbox',autofocus:'',disabled:false,required: true },
+					{key: 'radio',type: 'radio',col:'col-md-4',inputSizeClass:"input-sm",label: 'Radio',options:[{"name":"perro","value":"1"},{"name":"gato","value":"2"}],placeholder: 'Checkbox',autofocus:'',required: true },
 					{key: 'rango',type: 'range',col:'col-md-4',label: 'Slider',min:100,max:500,placeholder: 'Slider',autofocus:'',required: true },
 					
 					
